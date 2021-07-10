f07f0d016c262519da60cc6ffb5d84aec93737df
FrancoMRuiz <x.francoruiz@gmail.com>
2021-07-10 00:29:26 -0300

    add styles and google fonts

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,5 +1,6 @@
+/* eslint-disable @next/next/no-page-custom-font */
 import React, { Fragment } from "react";
-import Link from "next/link";
+import Head from "next/head";
 import { Header } from "./Header";
 import { Global, css } from "@emotion/react";
 
@@ -24,6 +25,8 @@ export const Layout = (props) => {
           }
           body {
             font-size: 1.6rem; /*16px*/
+            line-height: 1.5;
+            font-family: "PT Sans", sans-serif;
           }
           h1,
           h2,
@@ -31,16 +34,39 @@ export const Layout = (props) => {
             margin: 0 0 2rem 0;
             line-height: 1.5;
           }
+          h1,
+          h2 {
+            font-family: "Roboto Slab", serif;
+            font-weight: 700;
+          }
+          h3 {
+            font-family: "PT Sans", sans-serif;
+          }
           ul {
             list-style: none;
             margin: 0;
             padding: 0;
           }
           a {
-              text-decoration: none;
+            text-decoration: none;
           }
         `}
       />
+      <Head>
+        <title>Product Hunt</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          referrerpolicy="no-referrer"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@100;200;300;400;500;600;700;800;900&display=swap"
+          rel="stylesheet"
+        />
+      </Head>
       <Header />
       <main>{props.children}</main>
     </Fragment>
